2ca67ec8d616775f4329435980ce2e19dc845dcb
Bramcr <crBram98@gmail.com>
2026-01-19 01:01:02 +0100

    styling setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import "./styles/globals.css";
 import Home from "./pages/Home";
 
 function App() {
