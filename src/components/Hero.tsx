dfb8dbc06e11726d5233a7ff788e24401f22ca30
Bramcr <crBram98@gmail.com>
2026-01-20 15:25:40 +0100

    add typography component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import LCDScreen from "./LCDScreen"
+import Typography from "./ui/Typography"
 
 const Hero = () => {
   return (
@@ -6,7 +7,7 @@ const Hero = () => {
       <div className="container pt-26! md:pt-36!">
         <div className="mb-12 sm:mb-30">
           <h2 className="text-tx-light font-share-tech-mono text-base sm:text-xl md:text-2xl">FULL-STACK DEVELOPER</h2>
-          <h1 className="text-tx-light font-bold text-6xl sm:text-7xl md:text-9xl -ml-0.5 sm:-ml-1 md:-ml-1.5">BRAM C<span className="text-primary">.</span></h1>
+          <Typography variant="h1">BRAM C<span className="text-primary">.</span></Typography>
         </div>
         <div>
           <LCDScreen />
---
+++ src/components/ui/Typography.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+
+interface TypographyProps {
+  children: React.ReactNode;
+  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div';
+}
+
+const Typography = (props: TypographyProps) => {
+  const { children, variant } = props;
+
+  const variants = {
+    h1: 'text-tx-light font-bold text-6xl sm:text-7xl md:text-9xl -ml-0.5 sm:-ml-1 md:-ml-1.5',
+    h2: 'text-3xl font-bold',
+    h3: 'text-2xl font-bold',
+    h4: 'text-xl font-bold',
+    h5: 'text-lg font-bold',
+    h6: 'text-base font-bold',
+    p: 'text-base font-normal',
+    span: 'text-base font-normal',
+    div: 'text-base font-normal',
+  }
+
+  return (
+    <div className={cn(variants[variant])}>
+      {children}
+    </div>
+  )
+}
+
+export default Typography
