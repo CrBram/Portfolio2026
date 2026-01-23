cbf888baf3c1a71427ddf27c01aefb7cd1b15f91
Bramcr <crBram98@gmail.com>
2026-01-23 19:16:01 +0100

    red dot

--- src/components/Projects2.tsx
+++ src/components/Projects2.tsx
@@ -6,17 +6,17 @@ const Projects2 = () => {
       className="min-h-screen bg-cover bg-center bg-no-repeat bg-background relative overflow-hidden"
       style={{ backgroundImage: "url('/images/projects/orient_background.png')" }}
     >
-      <div className="absolute inset-0 bg-background-dark/17"></div>
+      <div className="absolute inset-0 bg-background-dark/15"></div>
       <div className="pt-24 relative z-10">
         <HorizontalProjectScroll
           text="ORIENT CONFIGURATOR"
-          imageSrc="/images/dot.png"
+          imageSrc="/images/red_dot.png"
           imageAlt="Dot"
           speed={10}
           duplicates={2}
         />
       </div>
-      <div className="container py-8! md:py-12! ">
+      <div className="container py-8! md:py-12!">
       </div>
     </section>
   )
