96ce3d45455515d35232288f15a7741f1afcf6e8
Bramcr <crBram98@gmail.com>
2026-01-23 20:01:31 +0100

    styling improvements

--- src/components/Projects2.tsx
+++ src/components/Projects2.tsx
@@ -1,4 +1,5 @@
 import HorizontalProjectScroll from './HorizontalProjectScroll'
+import Button from './ui/Button'
 
 const Projects2 = () => {
   return (
@@ -6,7 +7,7 @@ const Projects2 = () => {
       className="h-screen bg-cover bg-center bg-no-repeat bg-background relative overflow-hidden"
       style={{ backgroundImage: "url('/images/projects/orient_background.png')" }}
     >
-      <div className="absolute inset-0 bg-background-dark/15"></div>
+      <div className="absolute inset-0 bg-background-dark/20"></div>
       <div className="pt-24 relative z-10">
         <HorizontalProjectScroll
           text="ORIENT CONFIGURATOR"
@@ -17,11 +18,12 @@ const Projects2 = () => {
         />
       </div>
       <div className="container py-8! md:py-12! relative z-10">
-        <div className="flex justify-between pt-136">
-          <div className="">
+        <div className="flex flex-wrap justify-between md:pt-136 pt-120">
+          <div className="w-full md:w-auto">
             <p className="font-share-tech-mono text-tx-dark text-base sm:text-lg md:text-xl">SELECTED PROJECTS</p>
-            <p className="font-micro5 text-primary text-9xl -mt-6">#01</p>
+            <p className="font-micro5 text-primary text-8xl md:text-9xl -mt-6">#01</p>
           </div>
+          <Button className="w-full md:w-auto" text="VIEW PROJECT" onClick={() => { }} />
         </div>
       </div>
     </section>
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils"
+
+interface ButtonProps {
+  text: string
+  onClick: () => void
+  className?: string
+}
+
+const Button = (props: ButtonProps) => {
+  const { text, onClick, className } = props
+
+  return (
+    <div className={"flex items-center"}>
+      <button className={cn("bg-primary text-tx-black px-4 md:px-16 py-3 font-bold text-xl md:text-4xl flex items-center gap-6 cursor-pointer hover:bg-primary-hover hover:scale-102 transition-all duration-300", className)} onClick={onClick}>{text}
+        <img src="/images/icons/arrow.svg" alt="arrow" className="w-4 h-4 md:w-6 md:h-6" />
+      </button>
+    </div>
+  )
+}
+
+export default Button
