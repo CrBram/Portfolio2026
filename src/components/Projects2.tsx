b72e2284b9999d891fa83e558bb583e72ab723ef
Bramcr <crBram98@gmail.com>
2026-01-23 19:38:34 +0100

    wip projects

--- src/components/Projects2.tsx
+++ src/components/Projects2.tsx
@@ -3,7 +3,7 @@ import HorizontalProjectScroll from './HorizontalProjectScroll'
 const Projects2 = () => {
   return (
     <section
-      className="min-h-screen bg-cover bg-center bg-no-repeat bg-background relative overflow-hidden"
+      className="h-screen bg-cover bg-center bg-no-repeat bg-background relative overflow-hidden"
       style={{ backgroundImage: "url('/images/projects/orient_background.png')" }}
     >
       <div className="absolute inset-0 bg-background-dark/15"></div>
@@ -16,7 +16,13 @@ const Projects2 = () => {
           duplicates={2}
         />
       </div>
-      <div className="container py-8! md:py-12!">
+      <div className="container py-8! md:py-12! relative z-10">
+        <div className="flex justify-between pt-136">
+          <div className="">
+            <p className="font-share-tech-mono text-tx-dark text-base sm:text-lg md:text-xl">SELECTED PROJECTS</p>
+            <p className="font-micro5 text-primary text-9xl -mt-6">#01</p>
+          </div>
+        </div>
       </div>
     </section>
   )
