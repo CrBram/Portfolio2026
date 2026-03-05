3ad8051ae631bb0c2edd42fbb97aa11f39a73583
pgm-bramcriel <bramcrie@student.arteveldehs.be>
2026-03-05 13:35:52 +0100

    made hero more dynamic

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,24 @@
-import LCDScreen from "./LCDScreen"
-import Typography from "./ui/Typography"
+import LCDScreen from "./LCDScreen";
+import Typography from "./ui/Typography";
 
 const Hero = () => {
   return (
-    <section id="hero" className="bg-background-dark min-h-screen">
-      <div className="container pt-26! md:pt-36!">
-        <div className="mb-12 sm:mb-30">
-          <h2 className="text-tx-light font-share-tech-mono text-base sm:text-xl md:text-2xl">FULL-STACK DEVELOPER</h2>
-          <Typography variant="h1">BRAM C<span className="text-primary">.</span></Typography>
+    <section id="hero" className="bg-background-dark min-h-screen sm:flex">
+      <div className="container pt-26! md:pt-36! sm:flex sm:flex-1 sm:flex-col sm:justify-between">
+        <div className="mb-12 sm:mb-0">
+          <h2 className="text-tx-light font-share-tech-mono text-base sm:text-xl md:text-2xl">
+            FULL-STACK DEVELOPER
+          </h2>
+          <Typography variant="h1">
+            BRAM C<span className="text-primary">.</span>
+          </Typography>
         </div>
-        <div className="md:pb-6">
+        <div className="md:pb-16">
           <LCDScreen />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
